a0a35cc3a0537a2db9211e7ea0fffee04ac1d814
muskanbagrecha <muskanbagrecha04@gmail.com>
2024-07-21 23:58:32 +0530

    SOLID - Liskov substitution

---
+++ src/oops/SOLID/LiskovSubstitution/Rectangle.java
@@ -0,0 +1,13 @@
+package src.oops.SOLID.LiskovSubstitution;
+
+public class Rectangle {
+
+    public void setWidth(){
+
+    }
+
+    public void setHeight(){
+
+    }
+
+}
---
+++ src/oops/SOLID/LiskovSubstitution/Square.java
@@ -0,0 +1,5 @@
+package src.oops.SOLID.LiskovSubstitution;
+
+public class Square extends Rectangle{ //We consider square as a special type of rectangle
+
+}
